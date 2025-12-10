4b1032078fd312e3e7c28d65b40354d22ba0df47
mbeader <matt.beader@gmail.com>
2025-12-10 16:12:41 -0500

    shorten project names

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
-rmdir LMUSessionTrackerCore.Tests\TestResults /s /q & ^
-dotnet test LMUSessionTrackerCore.Tests --no-restore --collect:"XPlat Code Coverage" --settings LMUSessionTrackerCore.Tests\coverage.runsettings && ^
+rmdir Core.Tests\TestResults /s /q & ^
+dotnet test Core.Tests --no-restore --collect:"XPlat Code Coverage" --settings Core.Tests\coverage.runsettings && ^
 dotnet reportgenerator -reports:"*.Tests\TestResults\*\coverage.cobertura.xml" -targetdir:reports\coverage -historydir:reports\coveragehistory
