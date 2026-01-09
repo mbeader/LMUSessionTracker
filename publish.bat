cb59878678a38a64d07727578b96c02e444f5583
mbeader <matt.beader@gmail.com>
2026-01-08 20:24:36 -0500

    build angular when publishing

--- publish.bat
+++ publish.bat
@@ -1,4 +1,5 @@
 rmdir Client\out /s /q & ^
 rmdir Server\out /s /q & ^
 dotnet publish Client\Client.csproj -p:PublishProfile=FolderProfile -p:InterceptorsPreviewNamespaces=Microsoft.Extensions.Configuration.Binder.SourceGeneration && ^
+pushd Web && ng build && popd && ^
 dotnet publish Server\Server.csproj -p:PublishProfile=FolderProfile
